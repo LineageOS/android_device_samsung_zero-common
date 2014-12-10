c5e5ffb0fe20e6561907a20c2b3164e2311d060b
Ying Wang <wangying@google.com>
2014-12-10 12:44:33 -0800

    Set PRODUCT_AAPT_PREBUILT_DPI.
    The build system use both PRODUCT_AAPT_PREF_CONFIG and
    PRODUCT_AAPT_PREBUILT_DPI to select prebuilt dpi-specific apks.
    The first match takes precedence.
    
    Bug: 18388705
    Change-Id: Iba98af754c3397ddbb27287244db086cb4c43aa6
    (cherry picked from commit ba951f0e197e656442bdd2191deb1b4cd76c3d4d)

--- zero-common.mk
+++ zero-common.mk
@@ -31,10 +31,10 @@ endif
 # currently contain all of the bitmaps at 640dpi density so
 # we do this little trick to fall back to the xxhdpi version
 # if the 640dpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_CONFIG := xlarge
 PRODUCT_AAPT_PREF_CONFIG := 640dpi
 # A list of dpis to select prebuilt apk, in precedence order.
-PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
+PRODUCT_AAPT_PREBUILT_DPI := hdpi mdpi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
