ad3d826ce72bb73e838a570e4ef509273fa520c6
Brandon McAnsh <brandon.mcansh@gmail.com>
2017-01-03 16:09:04 -0500

    WIP: Convert to board/product fragments
    Change-Id: I0b0cdaa2657c99d326179f895d9be77c7f2b9818
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

---
+++ product/init.mk
@@ -0,0 +1,9 @@
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.samsungexynos7420 \
+    init.baseband.rc \
+    init.gps.rc \
+    init.samsungexynos7420.rc \
+    init.samsungexynos7420.usb.rc \
+    init.wifi.rc \
+    ueventd.samsungexynos7420.rc
