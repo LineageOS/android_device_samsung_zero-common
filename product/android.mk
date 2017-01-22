34be8095b17b601739d0a51e0ce870fb0c46ee15
Usaamah Patel <ussyp2015@gmail.com>
2017-01-22 11:47:58 +0000

    Enable Camera HAL3
    Change-Id: I8b63dd604e0b99ed4d65415590364ca287874f15
    Signed-off-by: Usaamah Patel <ussyp2015@gmail.com>

--- product/android.mk
+++ product/android.mk
@@ -1,3 +1,5 @@
 # First api level, device has been commercially launched
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.first_api_level=21
+    ro.product.first_api_level=21 \
+    ro.bq.gpu_to_cpu_unsupported=1 \
+    persist.camera.HAL3.enabled=1
