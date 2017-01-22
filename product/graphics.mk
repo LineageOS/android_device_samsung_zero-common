6e096c35c781e5a8fc6a6c98f78a0ac02579d32d
Usaamah Patel <ussyp2015@gmail.com>
2017-01-22 11:39:32 +0000

    Since we are using OMX blobs, Lets call Individual Modules
    Change-Id: I5d304361baa2a75959d79619a88dc8c2a937886d
    Signed-off-by: Usaamah Patel <ussyp2015@gmail.com>

--- product/graphics.mk
+++ product/graphics.mk
@@ -1,11 +1,24 @@
 PRODUCT_PACKAGES += \
     gralloc.exynos5
+    
+# hw composer HAL
+PRODUCT_PACKAGES += \
+    hwcomposer.exynos5
+
+# Memory Tracker HAL
+PRODUCT_PACKAGES += \
+    memtrack.exynos5
+
+# stagefright and device specific modules
+PRODUCT_PACKAGES += \
+    libstagefrighthw \
+    libExynosOMX_Core
+    
+# MobiCore
+PRODUCT_PACKAGES += \
+    mcDriverDaemon
+
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwc.force_gpu=1 \
     ro.bq.gpu_to_cpu_unsupported=1
-
-
-# call Samsung LSI board support package
-$(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
-$(call inherit-product, hardware/samsung_slsi-cm/exynos7420/exynos7420.mk)
