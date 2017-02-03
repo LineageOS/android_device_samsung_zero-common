4821b46b32adfd8431de277d44c8dd6b6e5b9f26
Jesse Chan <jc@linux.com>
2017-02-03 19:09:16 +0800

    zero: Proper handle of single-SIM variant
    Change-Id: Ida92eb5a3524dcebffc13217c5d0dcb70de15353
    Signed-off-by: Usaamah Patel <ussyp2015@gmail.com>

--- zero-common.mk
+++ zero-common.mk
@@ -182,10 +182,13 @@ PRODUCT_PACKAGES += \
 
 # Radio Properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.add_power_save=1 \
+	persist.radio.apm_sim_not_pwdn=1 \
     rild.libpath=/system/lib64/libsec-ril.so \
-    rild.libargs=-d /dev/ttyS0 \
+    rild.libpath2=/system/lib64/libsec-ril-dsds.so \
     ro.telephony.ril_class=SlteRIL \
     telephony.lteOnGsmDevice=1 \
+    telephony.lteOnCdmaDevice=0 \
     ro.telephony.default_network=9 \
     ro.use_data_netmgrd=false \
     persist.data.netmgrd.qos.enable=false \
