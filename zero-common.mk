5be1b769a6ac821636abac7ac92f5882c63f6487
Lukas0610 <mail@lukasberger.at>
2018-05-11 23:14:39 +0200

    zero: define DRM in HIDL manfiest + build service
    Change-Id: I980e8d06cd14df8dbfc0e8db2dcd99fa1ef212e4

--- zero-common.mk
+++ zero-common.mk
@@ -119,8 +119,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
-
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+    
 # DTB
 PRODUCT_PACKAGES += \
 	dtbhtoolExynos
