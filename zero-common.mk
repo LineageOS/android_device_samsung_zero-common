c0f1e73cb2e956f10c19b43dfe3dae11a15ea1a8
deadman96385 <seanhoyt963@gmail.com>
2018-05-19 16:12:46 -0400

    zero: Add a local aosp gnss hal
    Based on https://github.com/LineageOS/android_hardware_interfaces/commit/ad3ef076cfc948fd772ef2b442e40695eb4bdaba#diff-7545556340763c5ac6ea8ca5082a3655
    
    Change-Id: I814a5cd91c41d1420769f49197e2d4a549768e20
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- zero-common.mk
+++ zero-common.mk
@@ -145,7 +145,7 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-impl.exynos7420 \
     android.hardware.gnss@1.0-service
 
 # GPS
