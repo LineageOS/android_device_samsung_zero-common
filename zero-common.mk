d157e1b75cae50175f0f88db7291ab5fa6734f63
enesuzun2002 <4lukankalar@gmail.com>
2018-09-30 17:09:12 +0300

    zero: Remove libstlport
    Change-Id: Icef184fedcc2a03a999f5c1d65249d2c90f4f7f6

--- zero-common.mk
+++ zero-common.mk
@@ -183,10 +183,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
-# Libstlport
-PRODUCT_PACKAGES += \
-    libstlport
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@1.0-service-sdm \
