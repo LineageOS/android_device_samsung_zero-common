d3ad5a7d1f82be90762eef092eaabbfd30a56b12
Lukas0610 <mail@lukasberger.at>
2018-11-06 15:17:59 +0100

    zero: add device-specific bluetooth HIDL implementation
    Change-Id: I0a239cfd728fe3fcee4bbbb7f557becad666935a

--- zero-common.mk
+++ zero-common.mk
@@ -94,7 +94,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-impl.zero \
     android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
